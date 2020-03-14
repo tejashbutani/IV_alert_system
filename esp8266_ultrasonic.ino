ba02ee5b712d89dbc0bfae38902e472056875aed
Tejash Butani <tejasbutani78@gmail.com>
2020-03-14 14:08:09 +0530

    Update esp8266_ultrasonic.ino

--- esp8266_ultrasonic.ino
+++ esp8266_ultrasonic.ino
@@ -1,7 +1,26 @@
-void setup(){
+int lengthMeasured;
+long timeDuration; 
 
-  }
+int inputPin = 5;
+int outputPin = 4;
+
+void setup(){
+  pinMode(inputPin,INPUT);
+  pinMode(outputPin,OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Module Started!!");
+ }
 
 void loop(){
- 
+  digitalWrite(outputPin,LOW);
+  delay(20);
+  digitalWrite(outputPin,HIGH);
+   delay(100);
+  digitalWrite(outputPin,LOW);
+
+  timeDuration = pulseIn(inputPin, HIGH);
+  lengthMeasured = timeDuration*(3.4/2);
+  Serial.print("Length : ");
+  Serial.println(lengthMeasured);
+  delay(10);
   }
